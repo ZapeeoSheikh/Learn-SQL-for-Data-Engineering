905ee8d2a67fd016a19720c9658667cd7b60947a
Muhammad Rameez <77886136+ZapeeoSheikh@users.noreply.github.com>
2022-10-10 18:55:00 +0500

    Create null-isnull_DC.sql
    null, is null practice

---
+++ null-isnull_DC.sql
@@ -0,0 +1,20 @@
+
+
+/* Intermidiate SQL Queries - DataCamp */
+
+/* ===================================================== */
+
+select name from people 
+where deathdate is null
+
+/* ===================================================== */
+
+select title from films 
+where budget is null
+
+/* ===================================================== */
+
+select count(*) from films 
+where language is null;
+
+/* ===================================================== */
