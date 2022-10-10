335ba517093d78af72d9e014ab5fba8bccc0aa06
Muhammad Rameez <77886136+ZapeeoSheikh@users.noreply.github.com>
2022-10-10 19:02:18 +0500

    Create like-notLike.sql
    like, not like

---
+++ like-notLike.sql
@@ -0,0 +1,20 @@
+/* Intermideate SQL Queries - DataCamp */
+
+/* =========================================== */
+
+select name from people 
+where name like 'B%';
+
+
+/* =========================================== */
+
+select name from people 
+where name like '_r%';
+
+/* =========================================== */
+
+select name from people 
+where name not like 'A%';
+
+/* =========================================== */
+
