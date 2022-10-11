b5c6345e42ef4214672144b02fb17b1f311885d7
Muhammad Rameez <77886136+ZapeeoSheikh@users.noreply.github.com>
2022-10-11 16:54:36 +0500

    Create left_join.sql

---
+++ left_join.sql
@@ -0,0 +1,9 @@
+SELECT c1.name AS city, code, c2.name AS country,
+       region, city_proper_pop
+FROM cities AS c1
+  -- Join right table (with alias)
+  left JOIN countries AS c2
+    -- Match on country code
+    ON c1.country_code = c2.code
+-- Order by descending country code
+ORDER BY code DESC;
