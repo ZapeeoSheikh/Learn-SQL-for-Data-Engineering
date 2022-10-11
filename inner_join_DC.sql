df3efd65a251d02fa0a9617ea558572b1b176ff0
Muhammad Rameez <77886136+ZapeeoSheikh@users.noreply.github.com>
2022-10-11 15:15:09 +0500

    Create inner_join_DC.sql

---
+++ inner_join_DC.sql
@@ -0,0 +1,28 @@
+-- Select fields
+select c.code, c.name, c.region, p.year, p.fertility_rate
+  -- From countries (alias as c)
+from countries as c 
+  -- Join with populations (as p)
+inner join populations as p
+    -- Match on country code
+on p.country_code = c.code
+
+
+
+/* =============================================== */
+
+
+
+
+-- Select fields
+SELECT c.code, name, region, e.year, fertility_rate, unemployment_rate
+  -- From countries (alias as c)
+  FROM countries AS c
+  -- Join to populations (as p)
+  INNER JOIN populations AS p
+    -- Match on country code
+    ON c.code = p.country_code
+  -- Join to economies (as e)
+  INNER JOIN economies AS e
+    -- Match on country code and year
+  on c.code = e.code and e.year = p.year
