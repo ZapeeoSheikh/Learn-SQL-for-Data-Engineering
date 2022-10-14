475ed197c23ab4c648e0a6ebdcbcf4b306ef1816
Muhammad Rameez <77886136+ZapeeoSheikh@users.noreply.github.com>
2022-10-14 15:54:38 +0500

    Create subquery1.sql

---
+++ subquery1.sql
@@ -0,0 +1,18 @@
+/*
+SELECT countries.name AS country, COUNT(*) AS cities_num
+  FROM cities
+    INNER JOIN countries
+    ON countries.code = cities.country_code
+GROUP BY country
+ORDER BY cities_num DESC, country
+LIMIT 9;
+*/
+
+SELECT countries.name AS country,
+  -- Subquery
+  (SELECT count(*)
+   FROM cities
+   WHERE countries.code = cities.country_code) AS cities_num
+FROM countries
+ORDER BY cities_num desc, country
+LIMIT 9;
